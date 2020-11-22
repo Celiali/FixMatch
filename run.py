2d53a048a7d4b07293decc82f8033d0af0ddefba
Hui <huzh@kth.se>
2020-11-22 01:59:50 +0100

    run main

--- run.py
+++ run.py
@@ -11,66 +11,68 @@ from models import *
 from experiments import *
 from utils.utils import setup_default_logging
 
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(config_path='./config', config_name='config')
+def main(CONFIG: DictConfig) -> None:
+# # configuration
+# parser = argparse.ArgumentParser(description='Generic runner for FixMatch')
+# parser.add_argument('--config',  '-c',
+#                     dest="filename",
+#                     metavar='FILE',
+#                     help =  'path to the config file',
+#                     default='config/config.yaml')
+
+# args = parser.parse_args()
+# with open(args.filename, 'r') as file:
+#     try:
+#         config_file = yaml.safe_load(file)
+#     except yaml.YAMLError as exc:
+#         print(exc)
+# CONFIG = edict(config_file)
+    print('==> CONFIG is: \n', OmegaConf.to_yaml(CONFIG), '\n')
+
+    # initial logging file
+    logger = setup_default_logging(CONFIG, string = 'Train')
+    logger.info(CONFIG)
+
+    # # For reproducibility, set random seed
+    if CONFIG.Logging.seed == 'None':
+        CONFIG.Logging.seed = random.randint(1, 10000)
+    random.seed(CONFIG.Logging.seed)
+    np.random.seed(CONFIG.Logging.seed)
+    torch.manual_seed(CONFIG.Logging.seed)
+    torch.cuda.manual_seed_all(CONFIG.Logging.seed)
+    cudnn.deterministic = True
+    cudnn.benchmark = False
+
+    # get datasets
+    data = LOADDATA[CONFIG.DATASET.loading_data](CONFIG.DATASET)
+    labeled_training_dataset,unlabeled_training_dataset, valid_dataset, test_dataset = data.get_dataset()
+
+    # build wideresnet
+    model = WRN_MODELS[CONFIG.MODEL.name](CONFIG.MODEL)
+    logger.info("[Model] Building model {}".format(CONFIG.MODEL.name))
+
+    if CONFIG.EXPERIMENT.used_gpu:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = model.to(device=device)
+
+    experiment = EXPERIMENT[CONFIG.EXPERIMENT.name](model,CONFIG.EXPERIMENT)
+    experiment.labelled_loader(labeled_training_dataset)
+    if CONFIG.DATASET.loading_data != 'LOAD_ORIGINAL' and unlabeled_training_dataset != None:
+        experiment.unlabelled_loader(unlabeled_training_dataset, CONFIG.DATASET.mu)
+    experiment.validation_loader(valid_dataset)
+    experiment.fitting()
+    print("======= Training done =======")
+    logger.info("======= Training done =======")
+    experiment.test_loader(test_dataset)
+    experiment.testing()
+    print("======= Testing done =======")
+    logger.info("======= Testing done =======")
+
 if __name__ == '__main__':
     #### Error: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.###
     # import os
     # os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-    from omegaconf import DictConfig
-
-    @hydra.main(config_path='./config', config_name='config')
-    def main(CONFIG: DictConfig) -> None:
-    # # configuration
-    # parser = argparse.ArgumentParser(description='Generic runner for FixMatch')
-    # parser.add_argument('--config',  '-c',
-    #                     dest="filename",
-    #                     metavar='FILE',
-    #                     help =  'path to the config file',
-    #                     default='config/config.yaml')
-
-    # args = parser.parse_args()
-    # with open(args.filename, 'r') as file:
-    #     try:
-    #         config_file = yaml.safe_load(file)
-    #     except yaml.YAMLError as exc:
-    #         print(exc)
-    # CONFIG = edict(config_file)
-        print('==> CONFIG is: \n', CONFIG, '\n')
-
-        # initial logging file
-        logger = setup_default_logging(CONFIG, string = 'Train')
-        logger.info(CONFIG)
-
-        # # For reproducibility, set random seed
-        if CONFIG.Logging.seed == 'None':
-            CONFIG.Logging.seed = random.randint(1, 10000)
-        random.seed(CONFIG.Logging.seed)
-        np.random.seed(CONFIG.Logging.seed)
-        torch.manual_seed(CONFIG.Logging.seed)
-        torch.cuda.manual_seed_all(CONFIG.Logging.seed)
-        cudnn.deterministic = True
-        cudnn.benchmark = False
-
-        # get datasets
-        data = LOADDATA[CONFIG.DATASET.loading_data](CONFIG.DATASET)
-        labeled_training_dataset,unlabeled_training_dataset, valid_dataset, test_dataset = data.get_dataset()
-
-        # build wideresnet
-        model = WRN_MODELS[CONFIG.MODEL.name](CONFIG.MODEL)
-        logger.info("[Model] Building model {}".format(CONFIG.MODEL.name))
-
-        if CONFIG.EXPERIMENT.used_gpu:
-            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-            model = model.to(device=device)
-
-        experiment = EXPERIMENT[CONFIG.EXPERIMENT.name](model,CONFIG.EXPERIMENT)
-        experiment.labelled_loader(labeled_training_dataset)
-        if CONFIG.DATASET.loading_data != 'LOAD_ORIGINAL' and unlabeled_training_dataset != None:
-            experiment.unlabelled_loader(unlabeled_training_dataset, CONFIG.DATASET.mu)
-        experiment.validation_loader(valid_dataset)
-        experiment.fitting()
-        print("======= Training done =======")
-        logger.info("======= Training done =======")
-        experiment.test_loader(test_dataset)
-        experiment.testing()
-        print("======= Testing done =======")
-        logger.info("======= Testing done =======")
+    main()

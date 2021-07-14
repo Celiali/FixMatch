78aec80e0b2d4892d232d988bfb1ffdabb4a2a48
TomerMe <tomer.laor1@gmail.com>
2021-07-14 14:21:10 +0300

    Fix bug in SimpleColorCNN

--- models/simple_color_cnn.py
+++ models/simple_color_cnn.py
@@ -11,7 +11,8 @@ class SimpleColorCNN(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, params.num_classes)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, params.num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -27,5 +28,5 @@ if __name__ == '__main__':
     from easydict import EasyDict as edict
     params = {'depth':28, 'widen_factor':2, 'num_classes':10, 'dropout': 0.0}
     params = edict(params)
-    m = WideResNet(params)
+    m = SimpleColorCNN(params)
     print(m)
